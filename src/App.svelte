07209e8fc039ae35fe843b79576073d4b5fae6fa
chxry <moosejuicetex@gmail.com>
2021-06-13 12:01:27 +0100

    Rewrote in svelte

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import Score from "./Score.svelte";
+  const table = Array(16).fill(false);
+  let score = 0;
+  let localHighScore = parseInt(localStorage.getItem("highScore"));
+  let highScore = localHighScore ? localHighScore : 0;
+
+  const handlePieceClick = (e: MouseEvent,idx: number) => {
+    let piece = table[idx];
+    let target = <HTMLDivElement> e.target;
+    setTimeout(() => target.id = "", 50);
+    if(!piece) {
+      if (score > highScore) { 
+        highScore = score;
+        localStorage.setItem("highScore", highScore.toString());
+      }
+      score = 0;
+      target.id = "incorrect";
+      return;
+    }
+    target.id = "correct";
+    score++;
+    table[idx] = false;
+    table[getRandomIndex(idx)] = true;
+  }
+
+  const getRandomIndex = (not: number) => {
+    while (true) {
+      let idx = Math.floor(Math.random() * table.length);
+      if (!table[idx] && idx !== not) {
+        return idx;
+      }
+    }
+  }
+
+  for (var i = 0; i < 3; i++) {
+    table[getRandomIndex(0)] = true;
+  }
+</script>
+
+<main>
+  <div class="top">
+    <h1>Tapp</h1>
+    <Score label="SCORE" value={score} />
+    <Score label="HIGH-SCORE" value={highScore} />
+  </div>
+  <div class="game">
+    {#each Array(4) as _,row}
+      <div class="row">
+        {#each Array(4) as _,col}
+          <div on:click={(e) => handlePieceClick(e, row*4 + col)} class={["tile", table[row*4 + col] ? "active" : ""].join(" ")}/>
+        {/each}
+      </div>
+    {/each}
+  </div>
+</main>
---
+++ src/Score.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let label: string;
+  export let value: number;
+</script>
+
+<div class="score">
+  <h5>{label}</h5>
+  <h2>{value}</h2>
+</div>
+
+<style>
+  .score {
+    margin-left: 16px;
+  }
+
+  h2, h5 {
+    text-align: right;
+  }
+
+  h2 {
+    font-size: 40px;
+  } 
+
+  h5 {
+    font-weight: 400;
+  }
+</style>
